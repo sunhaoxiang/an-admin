52787483074723240941a9f778667df88b77b2a5
孙昊翔 <673686754@qq.com>
2023-04-11 22:17:10 +0800

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react'
+import { Form, Input, Button} from 'antd'
+import {} from '@ant-design/icons'
+import './Login.css'
+
 const Login = () => {
+  const [form] = Form.useForm()
+
+  function onFinish(values: any) {
+    console.log('Success:', values)
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+      <div className="login-form">
+        <div className="login-title">SOLAR ADMIN</div>
+        <Form
+          form={form}
+          name="login"
+          labelCol={{ span: 5 }}
+          size="large"
+          onFinish={onFinish}
+        >
+          <Form.Item>
+            <Input placeholder="Username" />
+          </Form.Item>
+          <Form.Item>
+            <Input placeholder="Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Login</Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   )
 }
