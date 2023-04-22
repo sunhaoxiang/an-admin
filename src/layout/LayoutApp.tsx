2abe29a1ced606a5ae429e46dce24deb8980c402
孙昊翔 <673686754@qq.com>
2023-04-22 10:50:53 +0800

    feat: layout

--- src/layout/LayoutApp.tsx
+++ src/layout/LayoutApp.tsx
@@ -1,22 +1,25 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import {Button, Result, Layout, Space } from 'antd'
+import {useAuth} from '@/hooks/useAuth'
+
+const {
+  Content,
+  Footer,
+  Header
+} = Layout
 
 function LayoutApp() {
-  const navigate = useNavigate()
-  function goHome() {
-    navigate('/')
-  }
-
-  return (
-    <div>
-      <div>header</div>
-      <div>sidebar</div>
-      <div>
-        <h2>hello</h2>
-        <Outlet />
-      </div>
-      <div>footer</div>
-    </div>
-  )
+  return <Space direction="vertical" size={[0, 48]} style={{width: '100%'}}>
+    <Layout>
+      <Header>Header</Header>
+      <Layout style={{padding: '0 24px'}}>
+        <Content>
+          <Outlet />
+        </Content>
+        <Footer>footer</Footer>
+      </Layout>
+    </Layout>
+  </Space>
 }
 
 export default LayoutApp
